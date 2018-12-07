1048da305ef7d0e9f86fc8ddde9bc09e0a1851e9
Yuki Jung <yukiej@gmail.com>
2018-12-06 16:35:20 -0800

    Create skeleton React components

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,10 +1,50 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Timechart from './components/timechart';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      history: {},
+      lastUpdated: ''
+    }
+  }
+  componentDidMount() {
+    //do the api call to coindesk
+    fetch('https://api.coindesk.com/v1/bpi/historical/close.json', {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log("the data looks like: ", data);
+      //update state
+      this.setState({
+        history: data.bpi, 
+        lastUpdated: data.time.updated
+      })
+    })
+  }
+
+  render() {
+    // console.log("timechart is ", Timechart);
+    return (
+        <div>
+          Last updated: {this.state.lastUpdated}
+          <div>
+            <Timechart/>
+          </div>
+          
+        </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <div>
-    I'm the seed of a React app!
-  </div>, 
+  <App/>, 
   document.getElementById('app')
-);
+)
 
+export default App;
