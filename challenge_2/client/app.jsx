dd9d81ea011956484233b6301939707fbcb23e90
Yuki Jung <yukiej@gmail.com>
2018-12-06 19:11:32 -0800

    Configure chart to look nicer

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -40,6 +40,10 @@ class App extends React.Component {
     var data = {
       labels: this.state.dates,
       datasets: [{
+        fill: false,
+        pointBackgroundColor: '#FF6E40',
+        pointBorderColor: '#FF9E80',
+        borderColor: '#FF9E80',
         data: this.state.BPIs
       }]
     }
@@ -47,13 +51,32 @@ class App extends React.Component {
       type: 'line',
       data: data,
       options: {
+        legend: {
+          display: false
+        },
         scales: {
           xAxes: [{
+            ticks: {
+              source: 'data'
+            },
             type: 'time',
+            time: {
+              unit: 'day'
+            },
             display: true,
             scaleLabel: {
               display: true,
               labelString: "Date",
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: "Bitcoin Price Index"
             }
           }]
         }
@@ -64,7 +87,7 @@ class App extends React.Component {
     return (
         <div>
           <h2>Last updated: {this.state.lastUpdated}</h2>
-          <h1>Bitcoin Price Index: Last 31 DAYS</h1>
+          <h1>Bitcoin Price Index Over Last 31 Days</h1>
           {/* <div>
             <Timechart/>
           </div> */}
